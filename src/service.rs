4270e40ccf6df1be98d35e6ee60711afc70d4752
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-24 14:31:38 +0100

    new: Defined Service structure with some basic defaults and load file ability.

---
+++ src/service.rs
@@ -0,0 +1,88 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+
+/*
+ * Service structure is a generic service representation.
+ */
+
+
+#[derive(RustcDecodable, Debug)]
+pub struct Service {
+
+    /* Veles: */
+    pub name: Option<String>,
+    pub dir: Option<String>,
+
+
+    /* Svarog: */
+    pub configure: Option<String>,
+    pub start: Option<String>,
+    pub after_start: Option<String>,
+    pub stop: Option<String>,
+    pub after_stop: Option<String>,
+    pub reload: Option<String>,
+    pub validate: Option<String>,
+
+
+    /* Perun: */
+    pub unix_socket: Option<String>,
+    pub pid_file: Option<String>,
+
+    /// listens is a vector of URLs like: ["127.0.0.1:1234", "1.2.3.4:5000"]
+    pub listens: Option<Vec<String>>,
+
+    /// domains is a vector of FQDN like: ["my.shiny.domain.com/page2?param=1", "some.com"]
+    pub domains: Option<Vec<String>>,
+}
+
+
+impl Service {
+
+    /// loads service definition from toml (ini) file
+    pub fn load(name: String) -> Result<String, String> {
+        let file_name = format!("/Services/{}", name.clone());
+        match File::open(file_name.clone()) {
+            Ok(mut file) => {
+                let mut buffer = String::new();
+                match file.read_to_string(&mut buffer) {
+                    Ok(_read_size) => {
+                        Ok(buffer.to_owned())
+                    },
+                    Err(error) => {
+                        Err(format!("Failed to read definition: {:?} {:?}", file, error))
+                    }
+                }
+            },
+            Err(cause) => {
+                Err(format!("Err for file: {:?}, cause: {:?}", file_name, cause))
+            }
+        }
+    }
+}
+
+
+impl Default for Service {
+    fn default() -> Service {
+        Service {
+            /* Veles: */
+            name: None,
+            dir: None,
+
+            /* Svarog: */
+            configure: None,
+            start: None,
+            after_start: None,
+            stop: None,
+            after_stop: None,
+            reload: None,
+            validate: None,
+
+            /* Perun: */
+            unix_socket: None,
+            pid_file: None,
+            listens: None,
+            domains: None,
+        }
+    }
+}
