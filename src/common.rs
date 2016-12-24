fb4feb7a5b384a817ec25c198aad57e922a06775
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-24 23:39:22 +0100

    imp: CHECK_INTERVAL decreased to 1s. SERVICES_GLOB - use ini extension (more common than toml yet both compliant)

--- src/common.rs
+++ src/common.rs
@@ -5,10 +5,10 @@ pub const NAME: &'static str = "Deities";
 pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 /// Veles endless loop pause interval
-pub static CHECK_INTERVAL: u64 = 2000;
+pub static CHECK_INTERVAL: u64 = 1000;
 
 /// Default dir containing services configuration
 pub static SERVICES_DIR: &'static str = "/Services";
 
 /// Default glob match for file types we want to process as services configuration
-pub static SERVICES_GLOB: &'static str = "*.toml";
+pub static SERVICES_GLOB: &'static str = "*.ini";
