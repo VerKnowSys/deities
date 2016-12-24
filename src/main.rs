0c37a6212267be87fe108857d6901f0bc130f671
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-24 16:19:40 +0100

    new: Implemented main loop in main. Also perform first availability reports.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 extern crate rustc_serialize;
 extern crate toml;
 extern crate glob;
+
 
 mod common;
 pub mod service;
@@ -11,48 +15,60 @@ pub mod veles;
 use toml::decode_str;
 use service::Service;
 use veles::Veles;
+use perun::Perun;
+
+use std::time::Duration;
+use std::thread::sleep;
+// use log::LogLevel;
 
 
 fn main() {
-    println!("Veles appeared.");
+    env_logger::init().unwrap();
 
-    for svce in Veles::list_services() {
-        match svce.unwrap().file_name() {
-            Some(astr) => {
-                match astr.to_str() {
-                    Some(service_file) => {
-                        match Service::load(service_file.to_string()) {
-                            Ok(service_ref) => {
-                                let val: Option<Service> = decode_str(service_ref.as_ref());
-                                match val {
-                                    Some(service) => {
-                                        println!("{:?}", service);
+    info!("Veles appeared!");
+    loop {
+        debug!("\n");
+        for svce in Veles::list_services() {
+            match svce.unwrap().file_name() {
+                Some(astr) => {
+                    match astr.to_str() {
+                        Some(service_file) => {
+                            match Service::load(service_file.to_string()) {
+                                Ok(service_ref) => {
+                                    let val: Option<Service> = decode_str(service_ref.as_ref());
+                                    match val {
+                                        Some(service) => {
+                                            // perfom Perun checks
+                                            match Perun::checks_for(service) {
+                                                Ok(ok) =>
+                                                    info!("{:?}", ok),
 
-                                    },
-
-                                    None => {
-                                        println!("{:?}", val);
+                                                Err(error) =>
+                                                    error!("{:?}", error),
+                                            }
+                                        },
+                                        None => {
+                                            error!("Failed to load service file: {:?}. Please double check definition syntax since we're not validating it properly for now.", service_file);
+                                        }
                                     }
+                                },
+                                Err(error) => {
+                                    error!("Error {:?}", error);
                                 }
-                            },
-
-                            Err(error) => {
-                                println!("Error {:?}", error);
                             }
+                        },
+                        None => {
+                            error!("No file?");
                         }
-                    },
-                    None => {
-                        println!("No file?");
                     }
+                },
+                None => {
+                    error!("No access to read file?");
                 }
-            },
-            None => {
-                println!("No access to read file?");
             }
         }
-
+        sleep(Duration::from_millis(2000));
     }
-    println!("Veles vanished.");
 }
 
 
