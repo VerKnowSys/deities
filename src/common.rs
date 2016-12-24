d02be904caf8dd53866e0940576980dfe50015d3
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-24 20:03:09 +0100

    new: Defined common mod with internal hardcodes.

---
+++ src/common.rs
@@ -0,0 +1,14 @@
+/// project name
+pub const NAME: &'static str = "Deities";
+
+/// project version from cargo metadata
+pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+/// Veles endless loop pause interval
+pub static CHECK_INTERVAL: u64 = 2000;
+
+/// Default dir containing services configuration
+pub static SERVICES_DIR: &'static str = "/Services";
+
+/// Default glob match for file types we want to process as services configuration
+pub static SERVICES_GLOB: &'static str = "*.toml";
