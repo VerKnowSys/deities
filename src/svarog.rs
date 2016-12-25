bbc240b03ed1148caabff19a0d2da3e860cf020f
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-25 13:31:42 +0100

    new: Extended Svarog with more fancy stuff. More in details.
    * Defined hostname()
    * Extended Service with Svarog trait
    * Improved Slack report formatting.

--- src/svarog.rs
+++ src/svarog.rs
@@ -1,44 +1,72 @@
-use slack_hook::{Slack, PayloadBuilder, AttachmentBuilder, Parse};
+use slack_hook::{Slack, PayloadBuilder, AttachmentBuilder, Parse, Field}; // SlackLink,
 use chrono::Local;
 use chrono::datetime::*;
-use slack_hook::SlackTextContent::{Text};
+use slack_hook::SlackTextContent::{Text}; // Link
+use hostname::get_hostname;
 
 use common::*;
+use service::Service;
 
 
 /*
  * Svarog is mr Smith - that can do variety of stuff
  */
-pub struct Svarog;
+pub trait Svarog {
+
+    fn hostname(&self) -> String;
+
+    fn notification(&self, message: String, error: String) -> Result<String, String>;
+
+}
 
 
-impl Svarog {
+impl Svarog for Service {
 
 
-    pub fn notification(message: String) -> Result<String, String> {
+    fn notification(&self, message: String, error: String) -> Result<String, String> {
         let local: DateTime<Local> = Local::now();
         let slack = Slack::new(SLACK_WEBHOOK_URL).unwrap();
         let p = PayloadBuilder::new()
             .attachments(
                 vec![
-                    AttachmentBuilder::new("Failure")
-                    .title("Problem notification")
+                    AttachmentBuilder::new(DEFAULT_NOTIFICATION_NAME)
+                    .title("ALERT NOTIFICATION")
+                    .author_name(DEFAULT_NOTIFICATION_NAME)
+                    .author_icon(DEFAULT_VKS_LOGO)
                     .color("#FF3d41")
                     .text(
                         vec![
-                          Text(message.into()),
-                          // Link(SlackLink::new("https://google.com", "Google")),
-                          // Text("".into())
+                            Text("Unstable service detected. Deities will attempt to solve this problem automatically.".into()),
+                            // Link(SlackLink::new("https://google.com", "Google")),
+                            Text("".into()),
                         ].as_slice())
+                    .fields(
+                        vec![
+                            Field::new("", "", Some(false)),
+                            Field::new("", "", Some(false)),
+                            Field::new("Service details:", self.to_string(), Some(true)),
+                            Field::new("Message:", message, Some(true)),
+                            Field::new("", "", Some(false)),
+                            Field::new("Host:", self.hostname(), Some(true)),
+                            Field::new("Type:", "Service", Some(true)),
+                            Field::new("", "", Some(false)),
+                            Field::new("Error cause:", error, Some(false)),
+                        ])
                     .ts(&local.naive_local())
+                    .footer_icon(DEFAULT_VKS_LOGO)
+                    .footer(vec![
+                        Text("© 2o16-2o17   |".into()),
+                        ].as_slice())
                     .build()
                     .unwrap()
                 ])
             .link_names(true)
             .unfurl_links(true)
             .unfurl_media(true)
-            .username("Svarog")
+            .username(DEFAULT_NOTIFICATION_NAME)
+            .icon_url(DEFAULT_VKS_LOGO)
             .icon_emoji(":rotating_light:")
+            .text("")
             .channel(SLACK_ALERT_CHANNEL)
             .parse(Parse::Full)
             .build()
@@ -50,6 +78,15 @@ impl Svarog {
                 Ok("Notifiication sent".to_string()),
             Err(cause) =>
                 Err(format!("Notification send failure: {:?}", cause)),
+        }
+    }
+
+
+    /// Helper to read hostname from underlring system
+    fn hostname(&self) -> String {
+        match get_hostname() {
+            Some(host) => host,
+            None => DEFAULT_HOSTNAME.to_string(),
         }
     }
 
