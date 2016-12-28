f877fb2876018a092e93cb23394bc492174082b3
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-28 10:20:11 +0100

    imp: Continue start_service() routines only if self.start is set.

--- src/veles.rs
+++ src/veles.rs
@@ -27,50 +27,49 @@ impl Veles for Service {
             Some(ref commands) => {
                 trace!("Built command line: {:?}", commands);
                 cmd.args(commands.as_slice());
+                cmd.current_dir(self.work_dir());
+
+                // NOTE: always set stdin to null:
+                cmd.stdin(Stdio::null());
+                cmd.stdout(Stdio::null());
+                cmd.stderr(Stdio::null());
+
+                match get_user_by_name(self.user().as_ref()) {
+                    Some(uid) => {
+                        trace!("Setting service UID of valid user: {:?}", uid.uid());
+                        cmd.uid(uid.uid());
+                    },
+                    None => {
+                        debug!("Username {} not found in system!", self.user())
+                    }
+                }
+
+                match get_group_by_name(self.group().as_ref()) {
+                    Some(gid) => {
+                        trace!("Setting service GID of valid group: {:?}", gid.name());
+                        cmd.gid(gid.gid());
+                    },
+                    None => {
+                        debug!("Username {} not found in system!", self.group())
+                    }
+                }
+
+                match cmd.spawn() {
+                    Ok(child) => {
+                        let pid = child.id();
+                        // match child.wait() {}
+                        sleep(Duration::from_millis(100));
+                        debug!("Service got pid: {}", pid);
+                        Ok(pid)
+                    }
+                    Err(e) => {
+                        error!("Failed to spawn command: {:?}. Reason: {}", cmd, e);
+                        Err("Failed".to_string())
+                    }
+                }
             },
-            None => warn!("No start commands defined."),
-        }
-
-        cmd.current_dir(self.work_dir());
-
-        // NOTE: always set stdin to null:
-        cmd.stdin(Stdio::null());
-        cmd.stdout(Stdio::null());
-        cmd.stderr(Stdio::null());
-
-        match get_user_by_name(self.user().as_ref()) {
-            Some(uid) => {
-                trace!("Setting service UID of valid user: {:?}", uid.uid());
-                cmd.uid(uid.uid());
-            },
-            None => {
-                debug!("Username {} not found in system!", self.user())
-            }
-        }
-
-        match get_group_by_name(self.group().as_ref()) {
-            Some(gid) => {
-                trace!("Setting service GID of valid group: {:?}", gid.name());
-                cmd.gid(gid.gid());
-            },
-            None => {
-                debug!("Username {} not found in system!", self.group())
-            }
-        }
-
-        match cmd.spawn() {
-            Ok(child) => {
-                let pid = child.id();
-                // match child.wait() {}
-                sleep(Duration::from_millis(100));
-                debug!("Service got pid: {}", pid);
-                Ok(pid)
-            }
-            Err(e) => {
-                error!("Failed to spawn command: {:?}. Reason: {}", cmd, e);
-                Err("Failed".to_string())
-            }
+            None =>
+                Err(format!("No start commands defined for service: {}", self)),
         }
     }
-
 }
