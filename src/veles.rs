6b76cf024cd673f7b05b707d4ac183e4431dc6c6
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-28 10:11:15 +0100

    new: Implemented first version of start_service() for Veles. More in details.
    * Blindly spawn child, expecting success of spawn.
    * Disconnect any stdin/out/err from master process (no ppid dependency)
    * Spawn any defined command in default shell.
    * Set uid/gid of spawned process

--- src/veles.rs
+++ src/veles.rs
@@ -1,17 +1,76 @@
-use glob::glob;
-use glob::Paths;
+use std::thread::sleep;
+use std::time::Duration;
+use std::process::{Command, Stdio};
+use std::os::unix::process::CommandExt;
+use users::{get_user_by_name, get_group_by_name};
+
 use common::*;
+use service::Service;
 
 
 /*
  * Veles is a service spawner deity
  */
-pub struct Veles;
+pub trait Veles {
+
+    fn start_service(&self) -> Result<u32, String>;
+
+}
 
 
-impl Veles {
+impl Veles for Service {
 
+    fn start_service(&self) -> Result<u32, String> {
+        let mut cmd = Command::new(DEFAULT_SHELL);
+        cmd.arg("-c");
+        match self.start {
+            Some(ref commands) => {
+                trace!("Built command line: {:?}", commands);
+                cmd.args(commands.as_slice());
+            },
+            None => warn!("No start commands defined."),
+        }
 
+        cmd.current_dir(self.work_dir());
 
+        // NOTE: always set stdin to null:
+        cmd.stdin(Stdio::null());
+        cmd.stdout(Stdio::null());
+        cmd.stderr(Stdio::null());
+
+        match get_user_by_name(self.user().as_ref()) {
+            Some(uid) => {
+                trace!("Setting service UID of valid user: {:?}", uid.uid());
+                cmd.uid(uid.uid());
+            },
+            None => {
+                debug!("Username {} not found in system!", self.user())
+            }
+        }
+
+        match get_group_by_name(self.group().as_ref()) {
+            Some(gid) => {
+                trace!("Setting service GID of valid group: {:?}", gid.name());
+                cmd.gid(gid.gid());
+            },
+            None => {
+                debug!("Username {} not found in system!", self.group())
+            }
+        }
+
+        match cmd.spawn() {
+            Ok(child) => {
+                let pid = child.id();
+                // match child.wait() {}
+                sleep(Duration::from_millis(100));
+                debug!("Service got pid: {}", pid);
+                Ok(pid)
+            }
+            Err(e) => {
+                error!("Failed to spawn command: {:?}. Reason: {}", cmd, e);
+                Err("Failed".to_string())
+            }
+        }
+    }
 
 }
