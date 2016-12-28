8e487afdbd91b68ac20955c66d6425b1041cbcdc
Daniel (dmilith) Dettlaff <daniel@centra.com>
2016-12-28 15:28:00 +0100

    new: Increased CHECK_INTERVAL to 10s. Also changed default status channel name for Slack notifications to #ops-status

--- src/common.rs
+++ src/common.rs
@@ -5,7 +5,7 @@ pub const NAME: &'static str = "Deities";
 pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 /// Veles endless loop pause interval
-pub static CHECK_INTERVAL: u64 = 2000;
+pub static CHECK_INTERVAL: u64 = 10000;
 
 /// timeouts for connection, transfer and dns cache for curl
 pub static URLCHECK_TIMEOUT: u64 = 3000;
@@ -29,7 +29,7 @@ pub static CACERT_PEM: &'static str = "/etc/ssl/cert.pem";
 pub static SLACK_WEBHOOK_URL: &'static str = "";
 
 /// default channel to post notifications
-pub static SLACK_ALERT_CHANNEL: &'static str = "#dev-status";
+pub static SLACK_ALERT_CHANNEL: &'static str = "#ops-status";
 
 /// default host to report as a fallback
 pub static DEFAULT_HOSTNAME: &'static str = "localhost";
